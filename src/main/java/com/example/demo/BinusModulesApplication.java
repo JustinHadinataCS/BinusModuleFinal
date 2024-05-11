35319479c37e674a245245cf731edbd52417b110
Adrian <adriannugrohobasuki@gmail.com>
2024-05-11 17:12:14 +0700

    a

---
+++ src/main/java/com/example/demo/BinusModulesApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BinusModulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinusModulesApplication.class, args);
+	}
+
+	@Controller
+	public class MyController {
+
+		@GetMapping("/index")
+		public String index() {
+			return "index";
+		}
+	}
+
+}
