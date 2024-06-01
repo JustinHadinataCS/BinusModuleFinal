545557f4f9a018296d92ccbef739dc86971747bb
Adrian <adriannugrohobasuki@gmail.com>
2024-06-01 19:33:44 +0700

    Added logout

--- BinusModules/src/main/java/com/example/demo/Security/config/SecurityConfiguration.java
+++ BinusModules/src/main/java/com/example/demo/Security/config/SecurityConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -21,7 +23,8 @@ public class SecurityConfiguration {
         }).authorizeHttpRequests((authorize) -> {
             ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)authorize.requestMatchers(new String[]{"/registration*", "/login*","/error"})).permitAll().anyRequest()).authenticated();
         }).httpBasic(withDefaults())
-                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/index", true).usernameParameter("email").permitAll());
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/index", true).usernameParameter("email").permitAll())
+                .logout(logout -> logout.invalidateHttpSession(true).clearAuthentication(true).logoutRequestMatcher(new AntPathRequestMatcher("/logout")));
         return (SecurityFilterChain)http.build();
     }
 
