fea13a9a9c5d99b9b9ff435175dbd55d86fb8620
Justin Hadinata <Justinhadinata.23@gmail.com>
2024-06-01 14:58:52 +0700

    Student ID, Repo, Get Function Update

--- BinusModules/src/main/java/com/example/demo/Student/Student.java
+++ BinusModules/src/main/java/com/example/demo/Student/Student.java
@@ -1,4 +1,54 @@
 package com.example.demo.Student;
 
 public class Student {
+    private Long id;
+    private String name;
+    private Integer student_id;
+
+    public Student(String name, Integer student_id) {
+        this.name = name;
+        this.student_id = student_id;
+    }
+
+    public Student(Long id, String name, Integer student_id) {
+        this.id = id;
+        this.name = name;
+        this.student_id = student_id;
+    }
+
+    public Student() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(Integer student_id) {
+        this.student_id = student_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", student_id=" + student_id +
+                '}';
+    }
 }
---
+++ BinusModules/src/main/java/com/example/demo/Student/StudentConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.Student;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student michael = new Student(
+                    2L, "Michael", 9090
+            );
+            repository.saveAll(
+                    List.of(michael)
+            );
+
+        };
+    }
+}
