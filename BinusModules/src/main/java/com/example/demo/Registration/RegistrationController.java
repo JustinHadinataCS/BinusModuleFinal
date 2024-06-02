4879e106d4e2f065634ab1211f9ddcfe1179bf3e
Adrian <adriannugrohobasuki@gmail.com>
2024-06-02 20:30:18 +0700

    name appear on login + frontend updates :>

--- BinusModules/src/main/java/com/example/demo/Registration/RegistrationController.java
+++ BinusModules/src/main/java/com/example/demo/Registration/RegistrationController.java
@@ -1,12 +1,15 @@
 package com.example.demo.Registration;
 
 import com.example.demo.AppUser.AppUser;
+import com.example.demo.AppUser.AppUserService;
 import com.example.demo.Registration.RegistrationRequest;
 import com.example.demo.Registration.RegistrationService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,14 +19,18 @@ import static com.example.demo.AppUser.AppUserRole.ADMIN;
 public class RegistrationController {
 
     private RegistrationService registrationService;
+    private AppUserService appUserService;
     private List<AppUser> appUsers = new ArrayList<>();
 
-    public RegistrationController(RegistrationService registrationService) {
+    public RegistrationController(RegistrationService registrationService, AppUserService appUserService) {
         this.registrationService = registrationService;
+        this.appUserService = appUserService;
     }
 
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model, Principal principal){
+        UserDetails userDetails = appUserService.loadUserByUsername((String) principal.getName());
+        model.addAttribute("userdetail", userDetails);
         return "index";
     }
 
